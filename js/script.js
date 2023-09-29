204e8d8c33953b09fd7fd8228c4c7d6f4fb4b1b8
Andreska <andreska_nayara@hotmail.com>
2023-09-29 10:00:19 -0300

    versão final layout

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("name");
+const lastNameInput = document.querySelector("#last-name");
+const emailInput = document.querySelector("email");
+const passwordInput = document.querySelector("#password");
+const freeBtn = document.querySelector("#free");
+const sendBtn = document.querySelector("#send");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    validate()
+})
+
+function validate (){
+
+    
+}
